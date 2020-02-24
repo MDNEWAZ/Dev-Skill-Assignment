154de11808354430867c863a3124a2f9c1fd6eee
Shah Newaz <shahnewaz385@gmail.com>
2020-02-24 13:34:18 +0600

    Asp.net Core MVC Web Application.

---
+++ DevSkill.Assignment2/DevSkill.Assignment2.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevSkill.Assignment2.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
